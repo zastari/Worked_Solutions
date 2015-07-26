70cfb2e5338e435393327826bb2c4944f20b4079
zastari <zastari@gmail.com>
2015-07-26 21:38:34 +0000

    Fixing typo in comment

--- The C Programming Language/Chapter 1/1-16.c
+++ The C Programming Language/Chapter 1/1-16.c
@@ -1,4 +1,4 @@
-/* KR 1-15: Revise the main routine of the longest-line program so it will
+/* KR 1-16: Revise the main routine of the longest-line program so it will
  * correctly print the length of arbitrarily long input lines, and as much
  * as possible of the text
  *
