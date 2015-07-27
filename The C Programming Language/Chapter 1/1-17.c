b702b0158ab34f08a519e50418c63c48718ca0af
zastari <zastari@gmail.com>
2015-07-27 00:27:28 +0000

    Support lines of arbitrary length

--- The C Programming Language/Chapter 1/1-17.c
+++ The C Programming Language/Chapter 1/1-17.c
@@ -1,16 +1,14 @@
 // KR 1-17: Write a program to print all input lines that are longer than 80 characters
-
 #include <stdio.h>
 
-#define PRINTLINE 80
-#define MAXLINE 1000    /* Limit includes appended \0 */
+#define PRINTLINE 81    /* Print lines 81 characters or longer */
 
 int getaline(char s[], int lim);
 
 int getaline(char s[], int lim) {
     int c, i;
 
-    for(i = 0; (i < lim-1) && ((c = getchar()) != EOF) && (c != '\n'); ++i) {
+    for(i = 0; (i < lim) && ((c = getchar()) != EOF) && (c != '\n'); ++i) {
         s[i] = c;
     }
     if(c == '\n') {
@@ -26,18 +24,16 @@ int getaline(char s[], int lim) {
 int main()
 {
     int len, accumulate;
-    char line[MAXLINE];
+    char line[PRINTLINE+1];     /* Include space for terminal \0 */
 
     accumulate = 0;
-    while((len = getaline(line, MAXLINE)) > 0) {      /* Calling getchar() once it returns EOF always returns EOF */
-        if((accumulate == 0) && (len > PRINTLINE+1))
+    while((len = getaline(line, PRINTLINE+1)) > 0) {      /* Calling getchar() once it returns EOF always returns EOF */
+        if( (accumulate == 1) || ((accumulate == 0) && (len > PRINTLINE)) )
             printf("%s", line);
 
         if(line[len-1] == '\n')
             accumulate = 0;
         else {
-            if(accumulate == 0)
-                putchar('\n');
             accumulate = 1;
         }
     }
